4703561a68b0c7bc2549d98b9068f00aaa562829
Syba789 <95074177+Syba789@users.noreply.github.com>
2022-04-19 19:22:53 +0000

    done

--- p6final.c
+++ p6final.c
@@ -1,46 +1,28 @@
-//what is a string?
-//array of characters terminated by a '\0'
 #include<stdio.h>
 void input_string(char *a)
 {
   printf("enter a string\n");
   scanf("%s",a);
 }
-int string_length(char *a)
-{
-  int i=0;
-  for(i=0; a[i]!='\0'; i++);
-  return i;
-}
-char str_copy(char *d,char *a)
-{
-  for(int i=0; a[i]!='\0'; i++)
-    {
-      d[i]= a[i];
-    }
-}
 char str_reverse(char *a)
 {
-  int length=string_length(a);
-  length-=1;char t;
-  for(int i=0; i<length/2; i++)
-  {
-    t=a[i];
-    a[i]=a[length-i];
-    a[length-i]=t;
-  }
+  int i,m=0;
+  for(i=0; a[i]!='\0'; i++)
+    m++;
+  return m;
 }
-void output(char *a)
+void output(char *a, char reversea)
 {
-  printf("the reverse id %s",a);
+  printf("the reverse of the string is:\n");
+  for(int i=reversea-1; a[i]!='\0'; i--)
+    printf("%c",a[i]);
 }
 int main()
 {
-  char a[100],d[100];
+  char a[100];
   input_string(a);
-  str_copy(d,a);
-  str_reverse(a);
-  output(a);
+  int reverse_a;
+  reverse_a=str_reverse(a);
+  output(a,reverse_a);
   return 0;
 }
-
